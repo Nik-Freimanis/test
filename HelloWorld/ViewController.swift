1bc857890d59ae191db731b50f7f05f979bb13a8
Freimanis <nik.freymanis@gmail.com>
2023-01-16 14:52:18 +0700

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nikita Freimanis on 16/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
